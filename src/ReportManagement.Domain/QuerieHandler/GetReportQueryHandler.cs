f7bfba072027aa527b9bb1501c43b53e8c77f4e8
Piotr Walacik <piotr.walacik@hotmail.com>
2021-12-02 20:56:36 +0100

    add new test

--- src/ReportManagement.Domain/QuerieHandler/GetReportQueryHandler.cs
+++ src/ReportManagement.Domain/QuerieHandler/GetReportQueryHandler.cs
@@ -1,5 +1,4 @@
-﻿using AutoMapper;
-using MassTransit;
+﻿using MassTransit;
 using ReportManagement.Application.Dtos;
 using ReportManagement.Application.Queries;
 using ReportManagement.Domain.Models;
@@ -9,19 +8,15 @@ namespace ReportManagement.Application.QuerieHandler
 {
     public class GetReportQueryHandler : IConsumer<IGetReportQuery>
     {
-        private IMapper _mapper;
         private IReadReportRepository _reportRepository;
-        public GetReportQueryHandler(IMapper mapper, IReadReportRepository reportRepository)
+        public GetReportQueryHandler(IReadReportRepository reportRepository)
         {
-            _mapper = mapper;
             _reportRepository = reportRepository;
         }
         public async Task Consume(ConsumeContext<IGetReportQuery> context)
         {
             ReportModel? reportModels = await _reportRepository.GetByIdAsync(context.Message.Id);
             await context.RespondAsync<ReportDto>(reportModels);
-            
-
         }
     }
 }
---
+++ test/ReportManagement.Infrastructure.Test/QuerieHandlerTests/GetReportQueryHandlerTests.cs
@@ -0,0 +1,32 @@
+﻿using MassTransit;
+using Moq;
+using NUnit.Framework;
+using ReportManagement.Application.Dtos;
+using ReportManagement.Application.QuerieHandler;
+using ReportManagement.Application.Queries;
+using ReportManagement.Domain.Repositorys;
+using System;
+using System.Threading.Tasks;
+
+namespace ReportManagement.Infrastructure.Test.QuerieHandlerTests
+{
+    internal class GetReportQueryHandlerTests
+    {
+        [Test]
+        public async Task GetReportQuery_GetReport()
+        {
+            //Arange
+            var _readReportRepositoryMock = new Mock<IReadReportRepository>();
+
+            var _commandMock = new Mock<ConsumeContext<IGetReportQuery>>();
+            _commandMock.Setup(x => x.Message.Id).Returns(Guid.NewGuid());
+            var handler = new GetReportQueryHandler(_readReportRepositoryMock.Object);
+
+            //Act
+            await handler.Consume(_commandMock.Object);
+
+            //Asert
+            _commandMock.Verify(x => x.RespondAsync<ReportDto>(It.IsAny<object>()), Times.Once);
+        }
+    }
+}
